959cd785f7c19b098cf2e08d9ba9c2908a82f98e
YahiaMahdy <477562022@alex3.moe.edu.eg>
2026-07-04 15:56:23 +0300

    fixed errors related to category

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const connectDB = require('./db/connect');
 const productsRouter = require('./routes/products.routes');
 const ordersRouter = require('./routes/orders.routes');
 const cartRouter = require('./routes/cart.routes');
+const categoryRouter = require('./routes/category.routes');
 const errorHandler = require('./middleware/errorHandler');
 const AppError = require('./utils/AppError');
 
@@ -17,6 +18,7 @@ app.use(mongoSanitize());
 app.use('/api/products', productsRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/category', categoryRouter);
 
 app.use((req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl}`, 404));
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product.model');
+const Category = require('../models/category.model');
 const asyncHandler = require('../middleware/asyncHandler');
 const AppError = require('../utils/AppError');
 
